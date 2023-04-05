5a15476286dcb5254973bbf1e95dbabc44d0d719
NEWYORK-123 <126648899+NEWYORK-123@users.noreply.github.com>
2023-04-05 06:19:02 +0000

    tét

---
+++ main.py
@@ -0,0 +1,12 @@
+#    LIBRARY APP 
+#    Scan Member ID
+#      Scan Book ISBN
+#    If the book is on loan Return the book
+#    else,
+#   get BorrowDate
+#    calculate DueDate = BorrowDate +7 days
+#   Add BorrowDate, DueDate, BookISBN and MemberID to the Loan Table
+
+def Display_Member(Membberi,MemberName):
+print(f"User number {MemberID} goes by the name { MemberName}")
+Display_Member(1123,"The King")
